083df8e209d2a9f14fe02c8b15331bdbd79569b1
luis vasquez <levb20@gmail.com>
2012-02-11 18:09:21 -0600

    commit

---
+++ assets/grocery_crud/themes/flexigrid/js/flexigrid-add.js
@@ -0,0 +1,106 @@
+$(function(){
+		$('.ptogtitle').click(function(){
+			if($(this).hasClass('vsble'))
+			{
+				$(this).removeClass('vsble');
+				$('#main-table-box').slideDown("slow");
+			}
+			else
+			{
+				$(this).addClass('vsble');
+				$('#main-table-box').slideUp("slow");
+			}
+		});		
+		
+		$("#FormLoading").ajaxStart(function(){
+			   $(this).show();
+		});
+		$("#FormLoading").ajaxStop(function(){
+			   $(this).fadeOut('slow');
+		});		
+		
+		$('#crudForm').submit(function(){
+			$(this).ajaxSubmit({
+				url: validation_url,
+				dataType: 'json',
+				cache: 'false',
+				success: function(data){
+					if(data.success)
+					{						
+						$('#crudForm').ajaxSubmit({
+							dataType: 'text',
+							cache: 'false',
+							success: function(result){
+								data = $.parseJSON( result );
+								if(data.success)
+								{	
+									$('#report-error').hide().html('');									
+									$('.field_error').each(function(){
+										$(this).removeClass('field_error');
+									});									
+									clearForm();
+									$('#report-success').html(data.success_message);
+									$('#report-success').slideDown('slow');
+								}
+								else
+								{
+									alert( message_insert_error );
+								}
+							},
+							error: function(){
+								alert( message_insert_error );
+							}
+						});
+					}
+					else
+					{
+						$('.field_error').each(function(){
+							$(this).removeClass('field_error');
+						});
+						$('#report-error').slideUp('fast');
+						$('#report-error').html(data.error_message);
+						$.each(data.error_fields, function(index,value){
+							$('input[name='+index+']').addClass('field_error');
+						});
+								
+						$('#report-error').slideDown('normal');
+						$('#report-success').slideUp('fast').html('');
+						
+					}
+				}
+			});
+			return false;
+		});
+	});	
+
+	function goToList()
+	{
+		if( confirm( message_alert_add_form ) )
+		{
+			window.location = list_url;
+		}
+
+		return false;	
+	}
+	
+	function clearForm()
+	{
+		$('#crudForm').find(':input').each(function() {
+	        switch(this.type) {
+	            case 'password':
+	            case 'select-multiple':
+	            case 'select-one':
+	            case 'text':
+	            case 'textarea':
+	                $(this).val('');
+	                break;
+	            case 'checkbox':
+	            case 'radio':
+	                this.checked = false;
+	        }
+	    });
+		
+		$('.remove-all').each(function(){
+			$(this).trigger('click');
+		});
+	}
---
+++ assets/grocery_crud/themes/flexigrid/js/flexigrid-edit.js
@@ -0,0 +1,71 @@
+$(function(){
+	$("#FormLoading").ajaxStart(function(){
+		   $(this).show();
+	});
+	$("#FormLoading").ajaxStop(function(){
+		   $(this).fadeOut('slow');
+	});	
+	
+	$('#crudForm').submit(function(){
+		$(this).ajaxSubmit({
+			url: validation_url,
+			dataType: 'json',
+			cache: 'false',
+			success: function(data){
+				if(data.success)
+				{						
+					$('#crudForm').ajaxSubmit({
+						dataType: 'text',
+						cache: 'false',
+						success: function(result){
+							data = $.parseJSON( result );
+							if(data.success)
+							{	
+								$('#report-error').hide().html('');									
+								$('.field_error').each(function(){
+									$(this).removeClass('field_error');
+								});									
+								
+								$('#report-success').html(data.success_message);
+								$('#report-success').slideDown('slow');
+							}
+							else
+							{
+								alert( message_update_error );
+							}
+						},
+						error: function(){
+							alert( message_update_error );
+						}
+					});
+				}
+				else
+				{
+					$('.field_error').each(function(){
+						$(this).removeClass('field_error');
+					});
+					$('#report-error').slideUp('fast');
+					$('#report-error').html(data.error_message);
+					$.each(data.error_fields, function(index,value){
+						$('input[name='+index+']').addClass('field_error');
+					});
+							
+					$('#report-error').slideDown('normal');
+					$('#report-success').slideUp('fast').html('');
+					
+				}
+			}
+		});
+		return false;
+	});
+});	
+
+function goToList()
+{
+	if( confirm( message_alert_edit_form ) )
+	{
+		window.location = list_url;
+	}
+
+	return false;	
+}
